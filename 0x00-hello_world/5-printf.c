6d7fa519ae197b5c75c809715a8677cb25a36fdd
MuchokiSE <snmuchoki@gmail.comgit config --global user.email snmuchoki@gmail.com>
2023-06-20 08:19:51 -0700

    C prints a particular string

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  *
- * Retun: Always 0,
+ * Return: Always 0,
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
-	return(0);
+	return (0);
 }
