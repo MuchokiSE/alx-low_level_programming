a677de053581804df00bcf35c6d294855ecbae2c
MuchokiSE <snmuchoki@gmail.com>
2023-06-25 14:40:24 -0700

    Prints a-z in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints a-z in lower case in new lines.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	for (char l = 'a'; l <= 'z'; l++)
+	{
+	putchar(l);
+	}
+	putchar('\n');
+
+	return (0);
+}
