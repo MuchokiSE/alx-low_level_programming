53c48ff30e1ba62a35591790f9309ab4ead2df7b
MuchokiSE <snmuchoki@gmail.com>
2023-06-26 09:15:54 -0700

    Print combis of single digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * description - print all combis of single digit nos. with new lines
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+	int j = 0;
+
+	for ( ; i < 10, j < 10; i++, j++)
+	{
+		for ( ; j < 10; j++)
+		{
+		putchar(i + '0');
+		putchar(j + '0');
+		putchar(',');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
