788b9005da5bb8259017b38d986d83e3d483bdef
MuchokiSE <snmuchoki@gmail.com>
2023-06-27 13:16:28 -0700

    Print combis of single digits

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,16 +9,12 @@
 int main(void)
 {
 	int i = 0;
-	int j = 0;
 
 	for ( ; i < 10; i++)
 	{
-		for ( ; j < 10; j++)
-		{
-		putchar(i + '0');
-		putchar(j + '0');
-		putchar(',' + ' ');
-		}
+	putchar(',');
+	putchar(' ');
+	putchar(i + '0');
 	}
 
 	putchar('\n');
