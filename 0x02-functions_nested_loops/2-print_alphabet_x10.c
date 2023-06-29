17e9795955d351f3edad6682e8fcb4736d293025
MuchokiSE <snmuchoki@gmail.com>
2023-06-28 18:48:32 -0700

    Print 10 the alphabet in lcase using _putchar

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -  prints 10 times the alphabet, in lowercase.
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+
+	char c = 'a';
+	{
+		for ( ; c <= 'z'; c++)
+		{
+		_putchar(c);
+		}
+	}
+	}
+
+	_putchar('\n');
+}
+
