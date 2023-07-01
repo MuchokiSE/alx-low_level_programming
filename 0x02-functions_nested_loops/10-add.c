929169c7f2a9fa8b04659d40aa40653390b2f28a
MuchokiSE <snmuchoki@gmail.com>
2023-07-01 06:42:25 -0700

    print sum of 2 ints

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -11,20 +11,7 @@
 int add(int i, int j)
 {
 	int k = add(i, j);
-
-	if (k >= -9 || k < 9)
 	{
-	_putchar(k + '0');
-	_putchar('\n');
+		return (k);
 	}
-
-
-	else
-	{
-	_putchar((k / 10) + '0');
-	_putchar((k % 10) + '0');
-	_putchar('\n');
-	}
-
-	return (0);
 }
