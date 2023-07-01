ccaa9f640696405905484c748077caa6c1a6d355
MuchokiSE <snmuchoki@gmail.com>
2023-07-01 11:20:26 -0700

    print n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,9 +6,7 @@
  */
 void print_times_table(int n)
 {
-	int i;
-	int j;
-	int k;
+	int i, j, k;
 
 	if (n >= 0 && n <= 14)
 	{
@@ -19,33 +17,30 @@ void print_times_table(int n)
 				k = i * j;
 				if (k > 99)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 100) + '0');
+				_putchar(((k / 10) % 10) + '0');
+				_putchar((k % 10) + '0');
 				}
-
 				else if (k > 9)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(((k / 10) % 10) + '0');
+				_putchar((k % 10) + '0');
 				}
-
 				else
 				{
 					if (j != 0)
 					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
 					}
-
-					_putchar(k + '0');
+				_putchar(k + '0');
 				}
 			}
 			_putchar('\n');
