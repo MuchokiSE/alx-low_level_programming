9b60c27126365d7bdb7099c0465c45a9f6fdf373
MuchokiSE <snmuchoki@gmail.com>
2023-07-10 05:46:35 -0700

    print tiangle using #

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,6 +8,7 @@ void print_triangle(int size)
 {
 	int i;
 	int j;
+	int k;
 
 	if (size <= 0)
 	{
@@ -16,10 +17,16 @@ void print_triangle(int size)
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j <= i; j++)
+		for (j = size - i; j > 1; j--)
+		{
+			_putchar(' ');
+		}
+		
+		for (k = 0; k <= i; k++)
 		{
 			_putchar('#');
 		}
+
 		_putchar('\n');
 	}
 }
