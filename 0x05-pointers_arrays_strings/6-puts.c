822073a9b5d863197ef8719761ae4990d86f27d7
MuchokiSE <snmuchoki@gmail.com>
2023-07-31 08:24:49 -0700

    print every other char in str

--- 0x05-pointers_arrays_strings/6-puts.c
+++ 0x05-pointers_arrays_strings/6-puts.c
@@ -7,12 +7,12 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int a = 0;
 
-	while (str[i] != '\0')
+	while (str[a] != '\0')
 	{
-		_putchar(str[i]);
-		i += 2;
+		_putchar(str[a]);
+		a += 2;
 	}
 	_putchar('\n');
 }
