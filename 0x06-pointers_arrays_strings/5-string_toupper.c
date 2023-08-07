88c992dcfd8358ab6b84ea5ac220e4fcaeb820e0
MuchokiSE <snmuchoki@gmail.com>
2023-08-07 08:17:36 -0700

    convert chars to uppercase in str

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *string_toupper - changes all lowercase letters of a string to uppercase.
+ *
+ *@str: string to be converted
+ *
+ * Return: 0.
+ */
+
+char *string_toupper(char *str)
+{
+	char *ptr = str;
+
+	while (*ptr != '\0')
+	{
+		if (*ptr >= 'a' && *ptr <= 'z')
+		{
+			*ptr = *ptr - 'a' + 'A';
+		}
+		ptr++;
+	}
+
+	return (str);
+}
