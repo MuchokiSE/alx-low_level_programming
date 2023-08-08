ea67cc3dca9966bc6d3a016bb6521bc4b6ffd30b
MuchokiSE <snmuchoki@gmail.com>
2023-08-08 06:52:57 -0700

    Capitalizes all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * *cap_string - Capitalizes all words of a string
+ *
+ *@str: string to be converted
+ *
+ * Return: str in capitalized form
+ */
+
+char *cap_string(char *str)
+{
+	int index = 0;
+	
+	while (str[index])
+	{
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+		
+		if (str[index - 1] == ' ' ||
+			str[index - 1] == '\t' ||
+			str[index - 1] == '\n' ||
+			str[index - 1] == ',' ||
+			str[index - 1] == '.' ||
+			str[index - 1] == ';' ||
+			str[index - 1] == '!' ||
+			str[index - 1] == '?' ||
+			str[index - 1] == '"' ||
+			str[index - 1] == '(' ||
+			str[index - 1] == ')' ||
+			str[index - 1] == '{' ||
+			str[index - 1] == '}' ||
+			index == 0)
+
+		{
+			if (str[index] >= 'a' && str[index] <= 'z')
+			{
+				str[index] = str[index] - 'a' + 'A';
+			}
+		}
+
+		index++;
+	}
+
+	return (str);
+}
